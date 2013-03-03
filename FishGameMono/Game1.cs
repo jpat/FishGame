99115cf2678c010cf755a90223094660d78308b0
Julia P <patterju@tcd.ie>
2013-03-03 16:39:19 +0000

    Add In-game screen

--- FishGameMono/Game1.cs
+++ FishGameMono/Game1.cs
@@ -23,24 +23,18 @@ namespace FishGameMono
         public static int screenWidth = 800;
         public static int screenHeight = 600;
 
-        public static int score;
-
         State currentState;
 
         public static SpriteFont font, roundedFont;
-
-        Texture2D sky, water, sand, coral;
-
-        List<Fish> fish;
-        List<Kelp> kelp;
 
         public KeyboardState lastKeyState;
 
         MenuScreen menu;
         GameOverNameScreen gameOver;
         HighScoreScreen highScores;
+        InGameScreen inGameScreen;
 
-        BoatFisherHook boat;
+        
 
         public static Rectangle screenRect;
 
@@ -58,16 +52,6 @@ namespace FishGameMono
         {
             currentState = State.Menu;
             screenRect = new Rectangle(0, 0, screenWidth, screenHeight);
-
-            fish = new List<Fish>();
-            kelp = new List<Kelp>
-            {
-                new Kelp(Kelp.KelpType.Multi, new Vector2(570, 380), 0.8f, 0.5f),
-                new Kelp(Kelp.KelpType.Single, new Vector2(150, 380), 0.8f, 0.5f),
-                new Kelp(Kelp.KelpType.Single, new Vector2(160, 390), 0.8f, 0.5f),
-                new Kelp(Kelp.KelpType.Single, new Vector2(140, 420), 0.8f, 0.5f),
-            };
-            
 
             base.Initialize();
         }
@@ -145,19 +129,19 @@ namespace FishGameMono
             BoatFisherHook.hookText = Content.Load<Texture2D>("hook");
             BoatFisherHook.lineText = Content.Load<Texture2D>("pixel");
             
-            sky = Content.Load<Texture2D>("sky");
-            water = Content.Load<Texture2D>("water");
-            sand = Content.Load<Texture2D>("sand2");
-            coral = Content.Load<Texture2D>("coral");
+            InGameScreen.sky = Content.Load<Texture2D>("sky");
+            InGameScreen.water = Content.Load<Texture2D>("water");
+            InGameScreen.sand = Content.Load<Texture2D>("sand2");
+            InGameScreen.coral = Content.Load<Texture2D>("coral");
 
             menu = new MenuScreen(roundedFont);
             gameOver = new GameOverNameScreen();
             highScores = new HighScoreScreen();
+            inGameScreen = new InGameScreen();
 
             MenuScreen.menuBG = Content.Load<Texture2D>("menubg");
             GameOverNameScreen.gameOverBG = Content.Load<Texture2D>("gameover");
             HighScoreScreen.highScoreBG = Content.Load<Texture2D>("scorefiller");
-            boat = new BoatFisherHook();
 
         }
 
@@ -182,11 +166,8 @@ namespace FishGameMono
                     break;
 
                 case(State.InGame):
-                    GameUpdate(gameTime);
-                    if (lastKeyState.IsKeyUp(Keys.K) && keyState.IsKeyDown(Keys.K))
-                    {
-                        currentState = State.GameOverName;
-                    }
+                    inGameScreen.Update(gameTime);
+                    currentState = inGameScreen.currentState;
                     break;
 
                 case(State.GameOverName):
@@ -211,73 +192,6 @@ namespace FishGameMono
             lastKeyState = keyState;
 
             base.Update(gameTime);
-        }
-
-        public void GameUpdate(GameTime gameTime)
-        {
-            
-            if (ShouldCreateNewFish())
-            {
-                fish.Add(Fish.CreateRandomFish());
-            }
-
-            foreach (Fish f in fish)
-            {
-                f.Update(gameTime);
-                if (BoxCollision(f, boat))
-                {
-                    /*if (IntersectPixels(new Rectangle(hookX, hookY, (int)(hook.Width * hookScale), (int)(hook.Height * hookScale)), hookData,
-                        new Rectangle((int)f.location.X, (int)f.location.Y, (int)(f.text.Width * f.scale), (int)(f.text.Height * f.scale)), f.data.colorData[0]))
-                    {*/
-
-                        boat.isLineFull = true;
-                        f.velocity = new Vector2(0.0f, -2.0f);
-                        f.isCaught = true;
-                        f.rotation = (float)(90 * Math.PI / 180);
-                        
-                   // }
-                }
-
-                if (f.AtBoat())
-                {
-                    switch (f.type)
-                    {
-                        case (Fish.FishType.Plain):
-                            score += 100;
-                            break;
-                        case (Fish.FishType.Pointy):
-                            score += 200;
-                            break;
-                        case (Fish.FishType.Jelly):
-                            score += 500;
-                            break;
-                        case (Fish.FishType.Crab):
-                            score += 1000;
-                            break;
-                        case (Fish.FishType.Boot):
-                            score -= 500;
-                            break;
-                    }
-                }
-            }
-
-            for (int i = fish.Count - 1; i >= 0; i--)
-            {
-                Fish f = fish.ElementAt(i);
-                if (f.IsOffScreen())
-                    fish.Remove(fish.ElementAt(i));
-            }
-
-            boat.Update(gameTime);
-        }
-
-        public static bool BoxCollision(GameObject obj1, GameObject obj2)
-        {
-            if (obj1.rect.Intersects(obj2.rect))
-            {
-                return true;
-            }
-            else return false;
         }
 
         /*static bool IntersectPixels(Rectangle rectangleA, Color[] dataA,
@@ -325,25 +239,7 @@ namespace FishGameMono
                     break;
 
                 case(State.InGame):
-
-                    spriteBatch.Draw(sky, new Vector2(0, -100), Color.White);
-                    spriteBatch.DrawString(roundedFont, "Score: " + score, new Vector2(10.0f, 25.0f), Color.DarkMagenta);
-
-                    spriteBatch.Draw(water, new Vector2(0, 200), Color.White);
-                    spriteBatch.Draw(coral, new Vector2(0, 200), new Color(200,200,200,200));
-                    spriteBatch.Draw(sand, new Vector2(0, screenHeight - sand.Height), Color.White);
-
-                    boat.Draw(gameTime, spriteBatch);
-
-                    foreach (Kelp k in kelp)
-                    {
-                        k.Draw(gameTime, spriteBatch);
-                    }
-
-                    foreach (Fish f in fish)
-                    {
-                        f.Draw(gameTime, spriteBatch);
-                    }
+                    inGameScreen.Draw(gameTime, spriteBatch);
                     break;
 
                 case(State.GameOverName):
@@ -362,11 +258,7 @@ namespace FishGameMono
             base.Draw(gameTime);
         }
 
-        private bool ShouldCreateNewFish()
-        {
-            Random r = new Random();
-            return r.Next(0, 1000) % 499 == 0;
-        }
+        
 
         /*private void PostHighScore(string name, int score)
         {
--- FishGameMono/Screens/InGameScreen.cs
+++ FishGameMono/Screens/InGameScreen.cs
@@ -1,22 +1,145 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FishGameMono.Screens
+namespace FishGameMono
 {
     class InGameScreen : GameScreen
     {
+        private KeyboardState lastKeyState;
+
+        private List<Fish> fish;
+        private List<Kelp> kelp;
+        private BoatFisherHook boat;
+
+        private int score;
+
+        public static Texture2D sky, sand, coral, water;
+
+        public InGameScreen()
+        {
+            currentState = State.InGame;
+            fish = new List<Fish>();
+            kelp = new List<Kelp>
+            {
+                new Kelp(Kelp.KelpType.Multi, new Vector2(570, 380), 0.8f, 0.5f),
+                new Kelp(Kelp.KelpType.Single, new Vector2(150, 380), 0.8f, 0.5f),
+                new Kelp(Kelp.KelpType.Single, new Vector2(160, 390), 0.8f, 0.5f),
+                new Kelp(Kelp.KelpType.Single, new Vector2(140, 420), 0.8f, 0.5f),
+            };
+            boat = new BoatFisherHook();
+        }
+        
         public override void Update(GameTime gameTime)
         {
-            
+            GameUpdate(gameTime);
+
+            KeyboardState keyState = Keyboard.GetState();
+            if (lastKeyState.IsKeyUp(Keys.K) && keyState.IsKeyDown(Keys.K))
+            {
+                currentState = State.GameOverName;
+            }
+        }
+
+        public void GameUpdate(GameTime gameTime)
+        {
+
+            if (ShouldCreateNewFish())
+            {
+                fish.Add(Fish.CreateRandomFish());
+            }
+
+            foreach (Fish f in fish)
+            {
+                f.Update(gameTime);
+                if (BoxCollision(f, boat))
+                {
+                    /*if (IntersectPixels(new Rectangle(hookX, hookY, (int)(hook.Width * hookScale), (int)(hook.Height * hookScale)), hookData,
+                        new Rectangle((int)f.location.X, (int)f.location.Y, (int)(f.text.Width * f.scale), (int)(f.text.Height * f.scale)), f.data.colorData[0]))
+                    {*/
+
+                    boat.isLineFull = true;
+                    f.velocity = new Vector2(0.0f, -2.0f);
+                    f.isCaught = true;
+                    f.rotation = (float)(90 * Math.PI / 180);
+
+                    // }
+                }
+
+                if (f.AtBoat())
+                {
+                    switch (f.type)
+                    {
+                        case (Fish.FishType.Plain):
+                            score += 100;
+                            break;
+                        case (Fish.FishType.Pointy):
+                            score += 200;
+                            break;
+                        case (Fish.FishType.Jelly):
+                            score += 500;
+                            break;
+                        case (Fish.FishType.Crab):
+                            score += 1000;
+                            break;
+                        case (Fish.FishType.Boot):
+                            score -= 500;
+                            break;
+                    }
+                }
+            }
+
+            for (int i = fish.Count - 1; i >= 0; i--)
+            {
+                Fish f = fish.ElementAt(i);
+                if (f.IsOffScreen())
+                    fish.Remove(fish.ElementAt(i));
+            }
+
+            boat.Update(gameTime);
+        }
+
+        private bool ShouldCreateNewFish()
+        {
+            Random r = new Random();
+            return r.Next(0, 1000) % 499 == 0;
+        }
+
+        public static bool BoxCollision(GameObject obj1, GameObject obj2)
+        {
+            if (obj1.rect.Intersects(obj2.rect))
+            {
+                return true;
+            }
+            else return false;
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
+            spriteBatch.Draw(sky, new Vector2(0, -100), Color.White);
+            spriteBatch.DrawString(Game1.roundedFont, "Score: " + score, new Vector2(10.0f, 25.0f), Color.DarkMagenta);
+
+            spriteBatch.Draw(water, new Vector2(0, 200), Color.White);
+            spriteBatch.Draw(coral, new Vector2(0, 200), new Color(200, 200, 200, 200));
+            spriteBatch.Draw(sand, new Vector2(0, Game1.screenHeight - sand.Height), Color.White);
+
+            boat.Draw(gameTime, spriteBatch);
+
+            foreach (Kelp k in kelp)
+                    {
+                        k.Draw(gameTime, spriteBatch);
+                    }
+
+                    foreach (Fish f in fish)
+                    {
+                        f.Draw(gameTime, spriteBatch);
+                    }
+                    
         }
 
     }
